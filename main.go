82ad0bf804f8fd6e6b77c39d77e3385dfa75031a
kumar vikram <kumarvikramshahi@gmail.com>
2022-10-26 15:11:54 +0530

    changes

--- main.go
+++ main.go
@@ -8,18 +8,6 @@ import (
 	"github.com/joho/godotenv"
 )
 
-// func main() {
-// 	// Hello world, the web server
-
-// 	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-// 		io.WriteString(w, "Hello, world!\n")
-// 	}
-
-// 	http.HandleFunc("/hello", helloHandler)
-// 	log.Println("Listing for requests at http://localhost:8000/hello")
-// 	log.Fatal(http.ListenAndServe(":8000", nil))
-// }
-
 func goDotEnvVariable(key string) string {
 
 	// load .env file
