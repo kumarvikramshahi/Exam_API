fbffe779b848d32e774cfa87606b2e3b748147b6
kumar vikram <kumarvikramshahi@gmail.com>
2022-10-26 15:09:32 +0530

    created project structures

--- main.go
+++ main.go
@@ -1,19 +1,51 @@
 package main
 
 import (
-	"io"
 	"log"
-	"net/http"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 )
 
-func main() {
-	// Hello world, the web server
+// func main() {
+// 	// Hello world, the web server
 
-	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Hello, world!\n")
+// 	helloHandler := func(w http.ResponseWriter, req *http.Request) {
+// 		io.WriteString(w, "Hello, world!\n")
+// 	}
+
+// 	http.HandleFunc("/hello", helloHandler)
+// 	log.Println("Listing for requests at http://localhost:8000/hello")
+// 	log.Fatal(http.ListenAndServe(":8000", nil))
+// }
+
+func goDotEnvVariable(key string) string {
+
+	// load .env file
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
 	}
 
-	http.HandleFunc("/hello", helloHandler)
-	log.Println("Listing for requests at http://localhost:8000/hello")
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	return os.Getenv(key)
+}
+
+func main() {
+	// Intit APP
+	app := fiber.New()
+
+	// Loading .env files
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+
+	// https://www.mongodb.com/docs/drivers/go/current/quick-start/ => for golang MongoDB tutorial
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Exam api")
+	})
+
+	app.Listen(":3000")
 }
